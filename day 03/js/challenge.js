fd43d8eaf223eff33ca4faf0e34635adf3132a7f
jessicaysmall <93561575+jessicaysmall@users.noreply.github.com>
2022-01-18 21:12:22 -0800

    push

---
+++ day 03/js/challenge.js
@@ -0,0 +1,20 @@
+const myAlertValue = prompt('What is your name?');
+console.lgo(firtName)
+const nameArray 
+myPrompt = myAlertValue;
+const reverseUser = myAlertValue.();
+alert(reverseUser);
+
+
+
+let numberValue = prompt('Enter number value default of 10');
+let numberOther = prompt('ENter another provide of 10');
+
+let numberValueParse = parseInt(numberValue);
+let numberOtherParse = parseInt(numberOther);
+alert(numberValueParse + numberOtherParse);
+
+const numberValue = prompt('Enter number value default of 10');
+const numberOther = prompt('ENter another provide of 10');
+const thisNumber = parseInt(numberValue + numberOther);
+alert(thisNumber);
---
+++ day 03/js/main.js
@@ -0,0 +1,277 @@
+/*const myVariable = 'Hellow World'; //case sensitve
+const myvariable = 'Hello World'; //case sensitive
+const my_variable = 'Hello World'; // not the same as above
+
+/* this comments out entire lines of code
+const myVariable = 'Hellow World'; //case sensitve
+const myvariable = 'Hello World'; //case sensitive
+const my_variable = 'Hello World'; // not the same as a
+*/
+/*
+//primituve data types
+const myString = 'Hello World';//normal string
+const myStringB = "Hello World";//abnormal string
+const myStringC = `Hello World`; //template string
+const myNumber = 10;
+const myBoolean = true;
+
+//other primitives
+const myNull = null;
+const myUndefined = undefined;
+const mySymnbol = Symbols();// fancy unique identifier
+const myBigInt = BigInt(10); // big integer number
+
+//complex data types
+const myArray =[1, 2, 3, 4, 5];
+const myObject = {};
+
+//type casing in js
+//type casings in js
+//camelCase - usedfor variables
+//PascalCase used for classes
+//snake_case - used for fucntions
+//kebab-case - used for css classes - not used in js
+//CONSTANT_CASE -u sed for constants
+
+//**************** */
+/*
+const myVariable = 'Hello World';
+console.log(myVariable.length);//type of operator
+//variable declaration types
+// let, const , var
+
+let myVariable = 'hello'; // let can be reassigned but not redeclared
+const myConstant = 'hello'; // const cannot be nreassigned or redecred
+var myVar = 'hello'; //var can be reassiged and 
+
+//******************* */
+// operators ==, ===, !=, !==, >, <, >=, <=,
+// + - * / %
+//++ --
+//
+
+/*
+let myStringA = 'Buddy';
+let myTemplateString = 'Hello ${mystringA}';
+*/
+
+//useful string methods
+
+let myString = 'Hello World';
+undefined
+console.log(myString.lenght);
+VM1111:1 undefined
+undefined
+console.log(myString.length);
+VM1194:1 11
+undefined
+console.log(myString.substring(0 , 5);
+VM1609:1 Uncaught SyntaxError: missing ) after argument list
+console.log(myString.split(' ');
+VM1702:1 Uncaught SyntaxError: missing ) after argument list
+console.log(myString.split(' '));
+VM1711:1 (2) ['Hello', 'World']
+undefined
+console.log(myString.substring(0 , 5));
+
+VM1719:1 Hello
+undefined
+//trim whitespace
+
+console.log(' Hello   '.trim());
+VM1850:1 Hello
+
+//PopUp methoods
+
+//alert
+
+alert('Hello World');
+
+//onfirm
+
+confirm('are you sure?');
+true
+const myAlert = alert('Hello World');
+
+
+const myConfirmValue = confirm('Are you sure');
+
+//promt take a respone returns a string
+
+const myPromptValue = promt('What is your name?');
+VM2348:1 Uncaught ReferenceError: promt is not defined
+    at <anonymous>:1:23
+(anonymous) @ VM2348:1
+const myPromptValue = prompt('What is your name?');
+
+//asks you a questions
+
+const myPromptValue = prompt('What is your name?');
+
+
+let myString = 'Hello World';
+undefined
+console.log(myString.lenght);
+VM1111:1 undefined
+undefined
+console.log(myString.length);
+VM1194:1 11
+undefined
+console.log(myString.substring(0 , 5);
+VM1609:1 Uncaught SyntaxError: missing ) after argument list
+console.log(myString.split(' ');
+VM1702:1 Uncaught SyntaxError: missing ) after argument list
+console.log(myString.split(' '));
+VM1711:1 (2) ['Hello', 'World']
+undefined
+console.log(myString.substring(0 , 5));
+
+VM1719:1 Hello
+undefined
+//trim whitespace
+undefined
+console.log(' Hello   '.trim());
+VM1850:1 Hello
+undefined
+//PopUp methoods
+undefined
+//alert
+undefined
+alert('Hello World');
+undefined
+//onfirm
+undefined
+confirm('are you sure?');
+true
+const myAlert = alert('Hello World');
+
+undefined
+const myConfirmValue = confirm('Are you sure');
+undefined
+//promt take a respone returns a string
+undefined
+const myPromptValue = promt('What is your name?');
+VM2348:1 Uncaught ReferenceError: promt is not defined
+    at <anonymous>:1:23
+(anonymous) @ VM2348:1
+const myPromptValue = prompt('What is your name?');
+undefined
+//asks you a questions
+undefined
+const myPromptValue = prompt('What is your name?');
+
+undefined
+Math
+Math {abs: ƒ, acos: ƒ, acosh: ƒ, asin: ƒ, asinh: ƒ, …}E: 2.718281828459045LN2: 0.6931471805599453LN10: 2.302585092994046LOG2E: 1.4426950408889634LOG10E: 0.4342944819032518PI: 3.141592653589793SQRT1_2: 0.7071067811865476SQRT2: 1.4142135623730951abs: ƒ abs()acos: ƒ acos()acosh: ƒ acosh()asin: ƒ asin()asinh: ƒ asinh()atan: ƒ atan()atan2: ƒ atan2()atanh: ƒ atanh()cbrt: ƒ cbrt()ceil: ƒ ceil()clz32: ƒ clz32()cos: ƒ cos()cosh: ƒ cosh()exp: ƒ exp()expm1: ƒ expm1()floor: ƒ floor()fround: ƒ fround()hypot: ƒ hypot()imul: ƒ imul()log: ƒ log()log1p: ƒ log1p()log2: ƒ log2()log10: ƒ log10()max: ƒ max()min: ƒ min()pow: ƒ pow()random: ƒ random()round: ƒ round()sign: ƒ sign()sin: ƒ sin()sinh: ƒ sinh()sqrt: ƒ sqrt()tan: ƒ tan()tanh: ƒ tanh()trunc: ƒ trunc()Symbol(Symbol.toStringTag): "Math"[[Prototype]]: Object
+console.log(Math.floor(2,9));
+VM2593:1 2
+undefined
+console.log(Math.ceil(2,3));
+VM2693:1 2
+undefined
+console.log(Math.round(2.6));
+VM2816:1 3
+undefined
+console.log(Math.cosh(5, 6));
+VM2921:1 74.20994852478785
+undefined
+console.log(Math.min(2,400,599,22,55));
+VM3041:1 2
+undefined
+console.log(Math.min(400,599,22,55));
+
+VM3051:1 22
+undefined
+console.log(Math.max('a', 'b', 'c'));
+VM3168:1 NaN
+undefined
+console.log(Math.max(null, {}, 'abc', [], 1, 200)):
+VM3285:1 Uncaught SyntaxError: Unexpected token ':'
+console.log(Math.max(null, {}, 'abc', [], 1, 200));
+
+VM3294:1 NaN
+undefined
+console.log(Math.random());
+VM3385:1 0.48722427823306313
+undefined
+//returns decimals
+undefined
+console.log(Math.random(2));
+VM3597:1 0.6450591911968262
+undefined
+console.log(Math.random() *10);
+VM3715:1 7.352959390920384
+undefined
+console.log(Math.floor(Math.random() *10 + 1));
+VM3875:1 7
+undefined
+console.log(Math.floor(Math.random() *10 + 1));
+
+VM3881:1 10
+undefined
+console.log(Math.floor(Math.random() *10 + 1));
+
+VM3886:1 2
+undefined
+console.log(Math.floor(Math.random() *10 + 1));
+
+VM3891:1 8
+undefined
+console.log(Math.floor(Math.random() *10 + 1));
+
+VM3896:1 2
+undefined
+// random number between 1 and 10 but not including zero;
+undefined
+//usefuld ate methods
+undefined
+console.log(new Date());
+VM4121:1 Tue Jan 18 2022 20:23:03 GMT-0800 (Pacific Standard Time)
+undefined
+console.log (date());
+VM4202:1 Uncaught ReferenceError: date is not defined
+    at <anonymous>:1:9
+(anonymous) @ VM4202:1
+console.log (Date());
+VM4234:1 Tue Jan 18 2022 20:26:39 GMT-0800 (Pacific Standard Time)
+undefined
+// having "new" understands that it will create a new object
+undefined
+console.log(new Date(2020, 0 , 1));
+VM4626:1 Wed Jan 01 2020 00:00:00 GMT-0800 (Pacific Standard Time)
+undefined
+// date can have diffrent formats
+undefined
+console.log(dDate(2020, 06, 12));
+VM4803:1 Uncaught ReferenceError: dDate is not defined
+    at <anonymous>:1:9
+(anonymous) @ VM4803:1
+console.log((Date(2020, 07, 10));
+VM4897:1 Uncaught SyntaxError: missing ) after argument list
+console.log(Date(2020, 07, 10));
+VM5007:1 Tue Jan 18 2022 20:30:09 GMT-0800 (Pacific Standard Time)
+undefined
+myDate = new Date();
+Tue Jan 18 2022 20:30:49 GMT-0800 (Pacific Standard Time)
+console.log((myDate.getFullYear());
+VM5179:1 Uncaught SyntaxError: missing ) after argument list
+console.log(myDate.getFullYear());
+VM5256:1 2022
+undefined
+console.log(myDate.getDay());
+VM5386:1 2
+undefined
+console.log(myDate.getMinutes());
+VM5478:1 30
+undefined
+console.log((myDate.getTime());
+VM5534:1 Uncaught SyntaxError: missing ) after argument list
+console.log(myDate.getTime());
+VM5613:1 1642566649837
+undefined
+console.log(myDate.setMonth(myDate.getMonth() + 3);
+VM5774:1 Uncaught SyntaxError: missing ) after argument list
+console.log(myDate.setMonth(myDate.getMonth() + 3));
+VM5786:1 1650339049837
+undefined
+

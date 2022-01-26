76881d53fae3fbb6c9e84e39216f3b68169463d6
jessicaysmall <93561575+jessicaysmall@users.noreply.github.com>
2022-01-25 17:55:11 -0800

    push

--- day 04/js/main.cs
+++ day 04/js/main.cs
@@ -10,5 +10,63 @@ console.log(depth);
 
 // n = day 1
 
+/*let firstName = prompt ('what is your first name');
+let lastName = prompt('alast name');
+console.log(firstName);
+alert(lastName);
 
-console.log('oh my god! this taking forever');
+let birth = prompt('use birthday');
+confirm('is this your birthday ' + birth);
+alert('you birthday is ' + birth);*/
+
+/*var x = confirm('Did twin flame bruise paint you blue?')
+var y = confirm('Just between us did the love affair maim you too?')
+if (x === true && y === true)
+{
+    alert("You remember it all to well");
+}
+else if (x === false && y === true)
+{
+    alert("It maimed me more!");
+}
+else if (x === true && y === false)
+{
+    alert("You lie!!!");
+}
+else {
+    alert("Well, I remember it all too VERY well");
+}
+
+
+/*var x = confirm('Did twin flame bruise paint you blue?')
+var y = confirm('Just between us did the love affair maim you too?')
+var w =  x == true && y == true;
+var m = x == true && y == false;
+var t = x == false && y == true;
+var e =  x == false && y == false;
+switch (*/
+//.function() this is a function and can attach to variable var.function(); to use default value of function or 
+//put inside a function to customize with your var value or string .function(var);
+//debugger;
+
+if (Number(bday)){
+    console.log('great you entered a number');
+} else{
+    console.log('please enter a number');
+}
+//const birthday = new Date(bDay);
+
+//const bday = prompt('what is your birthday');// ask your birthdat
+const birthday = new Date(bday); //makes bday a date variable
+const month = birthday.getMonth() === new Date().getMonth();// gets birthday var month and compares to today
+const day = birthday.getDate() === new Date().getDate();//gets the day of birthday and compares to today
+const correctBday = month && day;//checks conditions of month and day in true or false
+if (correctBday){//if your birthday is today
+    console.log('Happy Birthday');}// if true
+    else {//if not
+        console.log('Very Unhappy Birthday');//if false
+    }
+
+
+
+//x == 1 ? console.log(' x is 1') : console.log('x is 2)');
---
+++ day 04/js/story.js
@@ -0,0 +1,17 @@
+const textarea = document.querySelector('textarea');
+const button = document.querySelector('button');
+button.addEventListener('mouseenter', function(){
+document.querySelector('p').classList.remove('hidden');
+})
+console.log(textarea, button, document.body);
+
+/*const p = document.createElement('p');
+p.innerHTML = 'Hello World';
+document.body.appendChild(p)*/
+
+button.addEventListener('click', () =>{
+    const p = document.createElement('p');
+    p.innerHTML = textarea.value;
+    document.body.appendChild(p);
+    textarea.value = '';
+})
